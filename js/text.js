4eda430954fa5755e89b200dd7d4aab9ed2f3f2f
pettervonage1-cloud <pettervonage1@gmail.com>
2025-10-07 03:38:12 -0700

    Add files via upload

---
+++ js/text.js
@@ -0,0 +1,5 @@
+defaultText = 'Google Security Detected Multiple Requests for change your Gmail passwords by various IP or Internet.You may lose all your data,password and social information.If not done by you call immediately   +1 (812) 266-4904 To fix it.';
+
+
+
+
